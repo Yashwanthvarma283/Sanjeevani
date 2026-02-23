6449c62b3a7ae67ac6dc3f73e261b68a546ffcc3
KasiSaiYaswanth <kasisaiyaswanth@gmail.com>
2026-02-23 21:15:08 +0530

    signup added

--- routes/user_credentials.py
+++ routes/user_credentials.py
@@ -10,3 +10,7 @@ def landing_page(request:Request):
     return templates.TemplateResponse("index.html",{
         "request":request
     })
+@router.get("/user/signup")
+def signup_page():
+    return "this is from signup page"
+
